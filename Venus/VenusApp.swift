ace7601d3a56640feebefcdbbee604993daf6867
Kauã Vinicius <kauavsouza910@gmail.com>
2025-12-14 13:12:22 -0300

    Initial Commit

---
+++ Venus/VenusApp.swift
@@ -0,0 +1,17 @@
+//
+//  VenusApp.swift
+//  Venus
+//
+//  Created by Kaua on 14/12/25.
+//
+
+import SwiftUI
+
+@main
+struct VenusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
